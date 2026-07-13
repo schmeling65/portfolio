0c491bf715a0c227377618e541e08c96681cf58c
schmeling65 <markusrumpel2@gmail.com>
2026-07-13 16:44:32 +0200

    <> removed in the config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
 export default defineConfig({
-  site: 'https://<schmeling65>.github.io',
+  site: 'https://schmeling65.github.io',
   base: '/portfolio',
   output: 'static',
   vite: {
