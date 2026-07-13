6b3732949061b04f199e89c0a3903d3e651b07c6
schmeling65 <markusrumpel2@gmail.com>
2026-07-13 16:40:17 +0200

    added deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
 export default defineConfig({
+  site: 'https://<schmeling65>.github.io',
+  base: '/portfolio',
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
